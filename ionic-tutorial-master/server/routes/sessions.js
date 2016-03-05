6508d16ca1fbe666b8c09a4c788c8648afca07da
Darkness8642 <Dominick.Martelly@gmail.com>
2016-03-05 15:15:03 -0500

    Trying Images

--- ionic-tutorial-master/server/routes/sessions.js
+++ ionic-tutorial-master/server/routes/sessions.js
@@ -1,10 +1,12 @@
 var sessions = [
-
-    {id:0 , title:"Friends", speaker:"Feeds", time:"15", description: "Shitty time @ the beach"},
-    {id:1 , title:"Family", speaker:"Feeds", time:"10", description: "What up Fam"},
-    {id:2 , title:"Work", speaker:"Feeds", time:"5", description: "Yo, Jannet from Accounting is a bitch"},
-    {id:3 , title:"Mobile Performance Techniques", speaker:"JESSICA WONG", time:"3:10Pm", room:"Ballroom B", description: "In this session, you will learn performance techniques to speed up your mobile application."},
-    {id:4 , title:"Building Modular Applications", speaker:"LAURA TAYLOR", time:"2:00pm", room:"Ballroom A", description: "Join Laura to learn different approaches to build modular JavaScript applications."}
+    {id:0 , title:"Friends", speaker:"Feeds",
+        picture:"Friends.jpeg", description: "Shitty picture @ the beach"},
+    {id:1 , title:"Family", speaker:"Feeds",
+        picture:"Family.png", description: "What up Fam"},
+    {id:2 , title:"Work", speaker:"Feeds",
+        picture:"Work.jpeg", description: "Yo, Jannet from Accounting is a bitch"},
+    {id:3 , title:"Mobile Performance Techniques", speaker:"JESSICA WONG", picture:"3:10Pm", room:"Ballroom B", description: "In this session, you will learn performance techniques to speed up your mobile application."},
+    {id:4 , title:"Building Modular Applications", speaker:"LAURA TAYLOR", picture:"2:00pm", room:"Ballroom A", description: "Join Laura to learn different approaches to build modular JavaScript applications."}
 ];
 
 exports.findAll = function (req, res, next) {
