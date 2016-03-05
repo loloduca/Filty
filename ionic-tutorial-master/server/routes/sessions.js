8e29ac43eaa5e98a6264ab2047bab2229ae4197f
Marisol Bolivar <Marisol Bolivar>
2016-03-05 14:53:29 -0500

    updated thumnail w/ toggle

--- ionic-tutorial-master/server/routes/sessions.js
+++ ionic-tutorial-master/server/routes/sessions.js
@@ -1,7 +1,7 @@
 var sessions = [
-    {id:0 , title:"Introduction to Ionic", speaker:"CHRISTOPHE COENRAETS", time:"9:40am", room:"Ballroom A", description: "In this session, you'll learn how to build a native-like mobile application using the Ionic Framework, AngularJS, and Cordova."},
-    {id:1 , title:"AngularJS in 50 Minutes", speaker:"LISA SMITH", time:"10:10am", room:"Ballroom B", description: "In this session, you'll learn everything you need to know to start building next-gen JavaScript applications using AngularJS."},
-    {id:2 , title:"Contributing to Apache Cordova", speaker:"JOHN SMITH", time:"11:10am", room:"Ballroom A", description: "In this session, John will tell you all you need to know to start contributing to Apache Cordova and become an Open Source Rock Star."},
+    {id:0 , title:"Friends", speaker:"Feeds", time:"15", description: "Shitty time @ the beach"},
+    {id:1 , title:"Family", speaker:"Feeds", time:"10", description: "What up Fam"},
+    {id:2 , title:"Work", speaker:"Feeds", time:"5", description: "Yo, Jannet from Accounting is a bitch"},
     {id:3 , title:"Mobile Performance Techniques", speaker:"JESSICA WONG", time:"3:10Pm", room:"Ballroom B", description: "In this session, you will learn performance techniques to speed up your mobile application."},
     {id:4 , title:"Building Modular Applications", speaker:"LAURA TAYLOR", time:"2:00pm", room:"Ballroom A", description: "Join Laura to learn different approaches to build modular JavaScript applications."}
 ];
