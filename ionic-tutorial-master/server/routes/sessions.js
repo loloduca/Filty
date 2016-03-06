2eb49acb44fad67d30c6c0e82f23e457c78c5b85
Darkness8642 <Dominick.Martelly@gmail.com>
2016-03-06 08:18:05 -0500

    Another Small cleanup in About

--- ionic-tutorial-master/server/routes/sessions.js
+++ ionic-tutorial-master/server/routes/sessions.js
@@ -1,12 +1,12 @@
 var sessions = [
     {id:0 , title:"Friends", speaker:"Feeds",
-        picture:"Friends.jpeg", description: "Shitty picture @ the beach"},
+        picture:"Friends.jpeg", description: "Hey Doc... What's a Facebook??"},
     {id:1 , title:"Family", speaker:"Feeds",
         picture:"Family.png", description: "What up Fam"},
     {id:2 , title:"Work", speaker:"Feeds",
-        picture:"Work.jpeg", description: "Yo, Jannet from Accounting is a bitch"},
-    {id:3 , title:"Mobile Performance Techniques", speaker:"JESSICA WONG", picture:"3:10Pm", room:"Ballroom B", description: "In this session, you will learn performance techniques to speed up your mobile application."},
-    {id:4 , title:"Building Modular Applications", speaker:"LAURA TAYLOR", picture:"2:00pm", room:"Ballroom A", description: "Join Laura to learn different approaches to build modular JavaScript applications."}
+        picture:"Work.jpeg", description: "Yo, Jannet from Accounting is a b****"},
+    {id:3 , title:"Old Friends", speaker:"Feeds",
+        picture:"Old.jpeg", description: "..."}
 ];
 
 exports.findAll = function (req, res, next) {
