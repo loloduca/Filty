e6095b4956e198d8d93e6802fc88282d47bcdfec
Darkness8642 <Dominick.Martelly@gmail.com>
2016-03-05 22:20:24 -0500

    Added "Main Facebook API"

--- ionic-tutorial-master/responsible/www/js/app.js
+++ ionic-tutorial-master/responsible/www/js/app.js
@@ -9,88 +9,95 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB'])
 
 
-
-
-
-.run(function($ionicPlatform, ngFB) {
-  //ADD ITEM
+  .run(function ($ionicPlatform, ngFB) {
+    //ADD ITEM
     ngFB.init({appId: '1700608010209350'});
-  //ADD ITEM
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    //ADD ITEM
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    // .state('app.playlists', {
-    //   url: '/playlists',
-    //   views: {
-    //     'menuContent': {
-    //       templateUrl: 'templates/playlists.html',
-    //       controller: 'PlaylistsCtrl'
-    //     }
-    //   }
-    // })
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
 
-    .state('app.sessions', {
-      url: "/sessions",
-      views: {
+      .state('app.search', {
+        url: '/search',
+        views: {
           'menuContent': {
-              templateUrl: "templates/sessions.html",
-              controller: 'SessionsCtrl'
+            templateUrl: 'templates/search.html'
           }
-      }
-    })
+        }
+      })
 
-//DO NOT REMOVE!!!!!!!
-  .state('app.session', {
-    url: '/sessions/:sessionId',
-    views: {
-        'menuContent': {
-          templateUrl: "templates/session.html",
-          controller: 'SessionCtrl'
-      }
-    }
-});
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      // .state('app.playlists', {
+      //   url: '/playlists',
+      //   views: {
+      //     'menuContent': {
+      //       templateUrl: 'templates/playlists.html',
+      //       controller: 'PlaylistsCtrl'
+      //     }
+      //   }
+      // })
 
-  // if none of the above states are matched, use this as the fallback
-  // $urlRouterProvider.otherwise('/app/playlists');
-  $urlRouterProvider.otherwise('/app/sessions');
+      .state('app.sessions', {
+        url: "/sessions",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/sessions.html",
+            controller: 'SessionsCtrl'
+          }
+        }
+      })
 
-});
+      .state('app.feed', {
+        url: "/feed",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/feed.html",
+            controller: 'FeedController'
+          }
+        }
+      })
+
+      //DO NOT REMOVE!!!!!!!
+      .state('app.session', {
+        url: '/sessions/:sessionId',
+        views: {
+          'menuContent': {
+            templateUrl: "templates/session.html",
+            controller: 'SessionCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    // $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/sessions');
+
+  });
--- ionic-tutorial-master/responsible/www/js/controllers.js
+++ ionic-tutorial-master/responsible/www/js/controllers.js
@@ -1,85 +1,108 @@
-
 angular.module('starter.controllers', ['starter.services', 'ngOpenFB'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, ngFB) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, ngFB) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
 
-  $scope.fbLogin = function () {
-    ngFB.login({scope: 'email,user_posts,publish_actions'}).then(
+    $scope.fbLogin = function () {
+      ngFB.login({scope: 'email,user_posts,publish_actions'}).then(
         function (response) {
-            if (response.status === 'connected') {
-                console.log('Facebook login succeeded');
-                $scope.closeLogin();
-            } else {
-                alert('Facebook login failed');
-            }
+          if (response.status === 'connected') {
+            console.log('Facebook login succeeded');
+            $scope.closeLogin();
+          } else {
+            alert('Facebook login failed');
+          }
         });
+    };
+  })
 
-  };
+  .controller("FeedController", function ($scope, $http, $localStorage, $location) {
 
-})
+    $scope.feedData = [];
+    $scope.init = function () {
+      if ($localStorage.hasOwnProperty("accessToken") === true) {
+        $http.get("https://graph.facebook.com/v2.2/me/feed", {
+          params: {
+            access_token: $localStorage.accessToken,
+            format: "json"
+          }
+        }).then(function (result) {
+          $scope.feedData = result.data.data;
+          $http.get("https://graph.facebook.com/v2.2/me", {
+            params: {
+              access_token: $localStorage.accessToken,
+              fields: "picture",
+              format: "json"
+            }
+          }).then(function (result) {
+            $scope.feedData.myPicture = result.data.picture.data.url;
+          });
+        }, function (error) {
+          alert("There was a problem getting your profile.  Check the logs for details.");
+          console.log(error);
+        });
+      } else {
+        alert("Not signed in");
+        $location.path("/login");
+      }
+    };
+  })
 
-// .controller('PlaylistsCtrl', function($scope) {
-//   $scope.playlists = [
-//     { title: 'Reggae', id: 1 },
-//     { title: 'Chill', id: 2 },
-//     { title: 'Dubstep', id: 3 },
-//     { title: 'Indie', id: 4 },
-//     { title: 'Rap', id: 5 },
-//     { title: 'Cowbell', id: 6 }
-//   ];
-// })
+  // .controller('PlaylistsCtrl', function($scope) {
+  //   $scope.playlists = [
+  //     { title: 'Reggae', id: 1 },
+  //     { title: 'Chill', id: 2 },
+  //     { title: 'Dubstep', id: 3 },
+  //     { title: 'Indie', id: 4 },
+  //     { title: 'Rap', id: 5 },
+  //     { title: 'Cowbell', id: 6 }
+  //   ];
+  // })
 
-.controller('SessionsCtrl', function($scope, Session) {
+  .controller('SessionsCtrl', function ($scope, Session) {
     $scope.sessions = Session.query();
-})
+  })
 
-.controller('SessionCtrl', function($scope, $stateParams, Session) {
+  .controller('SessionCtrl', function ($scope, $stateParams, Session) {
     $scope.session = Session.get({sessionId: $stateParams.sessionId});
-})
-
-.controller('ClockCtrl', function($scope) {
-
-
-
-});
+  });
 
 // .controller('PlaylistCtrl', function($scope, $stateParams) {
 // });
